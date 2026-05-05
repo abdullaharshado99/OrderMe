19210171f2172cfcd73f773f549aefd06537ec42
Abdullah Arshad <abdullaharshado99@gmail.com>
2026-05-05 16:51:28 +0500

    updated endpoints by correct importing functions from ai pipeline.

--- ai_chatbot_pipeline/main.py
+++ ai_chatbot_pipeline/main.py
@@ -13,13 +13,13 @@ from apscheduler.executors.pool import ThreadPoolExecutor
 from apscheduler.schedulers.background import BackgroundScheduler
 from fastapi import FastAPI, Depends, HTTPException, BackgroundTasks
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from main import (
-    load_all_data, generated_chunk, query_rag,
-    delete_milvus_collection, delete_milvus_records,
-    AIPipelineFailureReasonEnum, AIPipelineStatusEnum,
-    insert_data_into_milvus_db, download_file_from_s3,
-    send_data_to_mongodb_as_webhook, get_tags_from_api
-)
+from ai_chatbot_pipeline.ai_pipeline.config import RAGConfig
+from ai_chatbot_pipeline.ai_pipeline.query_rag import Query
+from ai_chatbot_pipeline.ai_pipeline.doc_chunking import Chunk
+from ai_chatbot_pipeline.ai_pipeline.support_functions import Functions
+from ai_chatbot_pipeline.ai_pipeline.vector_storage import VectorDB
+from ai_chatbot_pipeline.ai_pipeline.media_download_cloud import Downloading
+from ai_chatbot_pipeline.ai_pipeline.data_extraction import DocumentProcessor
 
 load_dotenv()
 
@@ -103,62 +103,46 @@ async def run_pipeline_background(file_url: str, file_name: str, file_id: str, d
 
     try:
         logging.info(f"Pipeline execution started for file {file_name} ({file_id}) \n")
-        send_data_to_mongodb_as_webhook(file_id=[file_id], db=db_name, status=AIPipelineStatusEnum["IN_PROGRESS"],
-                                        access_token=token, total_tags={}, errors_list=[])
-
-        logging.info("Fetching keywords from server.")
-        tags = await get_tags_from_api(user_id, db_name, token)
-
-        if not tags:
-            error_list.append(AIPipelineFailureReasonEnum["TAGS_FETCHING_FAILED"])
-            send_data_to_mongodb_as_webhook(file_id=[file_id], db=db_name, status=AIPipelineStatusEnum["FAILED"],
-                                            access_token=token, total_tags={}, errors_list=error_list)
-            raise ValueError("Tags fetching failed!")
 
         logging.info(f"Step 1: Downloading file {file_name}: ({file_id}) from S3 bucket \n")
-        file_path, error = await download_file_from_s3(file_url, file_name)
+        file_path, error = await Downloading.download_file_from_s3(None, file_url, file_name)
 
         if error:
             error_list.append(error)
-            send_data_to_mongodb_as_webhook(file_id=[file_id], db=db_name, status=AIPipelineStatusEnum["FAILED"],
+            Functions.send_data_to_mongodb_as_webhook(file_id=[file_id], db=db_name, status=RAGConfig.AIPipelineStatusEnum["FAILED"],
                                             access_token=token, total_tags={}, errors_list=error_list)
             raise ValueError(f"Downloading failed for file: {file_name} {file_id}! \n")
 
         logging.info(f"Step 2: Loading data for {file_name} ({file_id}) \n")
-        documents, error = await load_all_data(file_id, file_path)
+        documents, error = await DocumentProcessor.load_all_data(None, file_id, file_path)
 
         if not documents:
             error_list.append(error)
-            error_list.append(AIPipelineFailureReasonEnum["CONTENT_EXTRACTION_FAILED"])
-            send_data_to_mongodb_as_webhook(file_id=[file_id], db=db_name, status=AIPipelineStatusEnum["FAILED"],
+            error_list.append(RAGConfig.AIPipelineFailureReasonEnum["CONTENT_EXTRACTION_FAILED"])
+            Functions.send_data_to_mongodb_as_webhook(file_id=[file_id], db=db_name, status=RAGConfig.AIPipelineStatusEnum["FAILED"],
                                             access_token=token, total_tags={}, errors_list=error_list)
             raise ValueError("No new data inserted during loading. \n\n")
 
         logging.info(f"Step 3: Generating chunks for {file_name} ({file_id}) \n")
-        chunks, metadata, error = await generated_chunk(documents)
+        chunks, metadata, error = await Chunk.generated_chunk(None, documents)
 
         if not chunks or not metadata:
             error_list.append(error)
-            error_list.append(AIPipelineFailureReasonEnum["CHUNKING_FAILED"])
+            error_list.append(RAGConfig.AIPipelineFailureReasonEnum["CHUNKING_FAILED"])
 
-            send_data_to_mongodb_as_webhook(file_id=[file_id], db=db_name, status=AIPipelineStatusEnum["PARTIAL"],
+            Functions.send_data_to_mongodb_as_webhook(file_id=[file_id], db=db_name, status=RAGConfig.AIPipelineStatusEnum["PARTIAL"],
                                             access_token=token, total_tags={}, errors_list=error_list)
             raise ValueError("Failed to generate chunks or metadata. \n\n")
 
         logging.info(f"Step 4: Inserting data into milvus database for {file_name} ({file_id}) \n")
-        file_ids, content, summary, total_tags, error, tag_error, consol_error = await insert_data_into_milvus_db(chunks, metadata, db_name,
-                                                                                         user_id, documents, tags)
-        if not summary or not total_tags or error:
+        file_ids, content, summary, error= await VectorDB.insert_data_into_milvus_db(chunks, metadata, db_name, user_id, documents)
+        if not summary or not error:
             error_list.append(error)
-            error_list.append(tag_error)
-            error_list.append(consol_error)
-            send_data_to_mongodb_as_webhook(file_id=[file_id], db=db_name, status=AIPipelineStatusEnum["PARTIAL"],
-                                            access_token=token, total_tags=total_tags, errors_list=error_list,
-                                            content=content, summary=summary)
+            Functions.send_data_to_mongodb_as_webhook(file_id=[file_id], db=db_name, status=RAGConfig.AIPipelineStatusEnum["PARTIAL"],
+                                            access_token=token, errors_list=error_list, content=content, summary=summary)
             raise ValueError("Failed to inserting data into milvus database. \n\n")
-        send_data_to_mongodb_as_webhook(file_id=[file_id], db=db_name, status=AIPipelineStatusEnum["COMPLETED"],
-                                        access_token=token, total_tags=total_tags, errors_list=[], content=content,
-                                        summary=summary)
+        Functions.send_data_to_mongodb_as_webhook(file_id=[file_id], db=db_name, status=RAGConfig.AIPipelineStatusEnum["COMPLETED"],
+                                        access_token=token, errors_list=[], content=content, summary=summary)
         logging.info(f"Pipeline execution completed for {file_name} ({file_id}) \n\n")
 
     except Exception as e:
@@ -195,7 +179,7 @@ async def execute_query(user_id: UserId,
                         auth_tuple: tuple[dict[str, Any], str] = Depends(verify_jwt_token)
                         ):
     try:
-        file_ids: List[str] = query_rag(user_id.id, db_name.name, user_query.query)
+        file_ids: List[str] = Query.query_rag(user_id.id, db_name.name, user_query.query)
         return {"file_ids": file_ids}
     except Exception as e:
         logging.error(f"Error in query execution: {e}")
@@ -210,7 +194,7 @@ async def delete_milvus_record_from_collection(
         auth_tuple: tuple[dict[str, Any], str] = Depends(verify_jwt_token)
 ):
     try:
-        response = delete_milvus_records(db_name.name, user_id.id, file_id.del_id)
+        response = VectorDB.delete_milvus_records(db_name.name, user_id.id, file_id.del_id)
         return JSONResponse(status_code=200, content={"message": response})
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -223,7 +207,7 @@ async def delete_collection(
         auth_tuple: tuple[dict[str, Any], str] = Depends(verify_jwt_token)
 ):
     try:
-        response = delete_milvus_collection(db_name.name, user_id.id)
+        response = VectorDB.delete_milvus_collection(db_name.name, user_id.id)
         return JSONResponse(status_code=200, content={"message": response})
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
